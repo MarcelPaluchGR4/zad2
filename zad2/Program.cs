9c539d251130abeb1ae41d6a35fe67fd2175c995
MarcelPaluchGR4 <parcelmurzyn@gmail.com>
2025-04-13 20:33:49 +0200

    WIP

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -11,10 +11,11 @@ class Object : ICloneable
 
     public string Parameter { get; set; }
     public double Rating { get; set; }
-    private static readonly Random random = new();
+    
 
     public Object(int LBnP)
     {
+        var random = new Random();
         StringBuilder sb = new(LBnP);
         for (int i = 0; i < LBnP; i++)
         {
@@ -49,12 +50,14 @@ class Object : ICloneable
         // ocenianie, tutaj zakladam ze beda tylko 2 parametry
         string part1 = Parameter.Substring(0, LBnP);
         string part2 = Parameter.Substring(LBnP, LBnP);
-        double value = bytesStringsValues[part1] + bytesStringsValues[part2];
-        Rating = value;
+        double x1 = bytesStringsValues[part1];
+        double x2 =  bytesStringsValues[part2];
+        Rating = Math.Sin(x1*0.05)+Math.Sin(x2*0.05)+0.4*Math.Sin(x1*0.15)*Math.Sin(x2*0.15);
     }
 
     public void Mutate()
     {
+        var random = new Random();
         //mutacja
         int randomIndex = random.Next(Parameter.Length);
         char[] charList = Parameter.ToCharArray();
@@ -64,16 +67,15 @@ class Object : ICloneable
             charList[randomIndex] = '0';
         Parameter = new string(charList);
     }
-
 }
 
 class Program
 {
-    static int LBnP = 3;
+    static int LBnP = 4;
     static int numberOfParameters = 3;
-    static int zdmin = -1;
-    static int zdmax = 1;
-    static int numberOfObjects = 100;
+    static int zdmin = 0;
+    static int zdmax = 100;
+    static int numberOfObjects = 13;
     static float tournamentSize = 0.2f;
 
     static List<string> GenerateBytesStrings(int n)
@@ -89,22 +91,6 @@ class Program
         return bytesStrings;
     }
 
-    public static List<Object> Cross(List<Object> objects)
-    {
-        Random random = new();
-        List<Object> crossed_list = new();
-        int b_cut = random.Next(LBnP*3 -2 );
-        Object specimen1 = objects[0];
-        Object specimen2 = objects[1];
-        Object specimen3 = objects[2];
-        Object specimen4 = objects[3];
-        Object specimen9 = objects[8];
-        Object specimen10 = objects[9];
-        Object firstFromLast = objects[objects.Count - 2];
-        Object last = objects[objects.Count -1];
-
-    }
-
     static Dictionary<string, double> GenerateValues(List<string> bytesStrings, int zdmax, int zdmin)
     {
         int range = zdmax - zdmin;
@@ -112,7 +98,7 @@ class Program
         Dictionary<string, double> mapping = [];
         for (int i = 0; i < bytesStrings.Count ; i++)
         {
-            mapping[bytesStrings[i]] = zdmin + step*i;
+            mapping[bytesStrings[i]] = (double)zdmin + step*i;
         }
         mapping[bytesStrings[bytesStrings.Count-1]] = zdmax; 
         return mapping;
@@ -148,7 +134,39 @@ class Program
                 bestObject = item;
             }
         } 
-        return new Object(bestObject.Parameter);
+
+        return new Object(bestObject);
+    }
+
+    public static Object Crossover(Object parent1, Object parent2)
+    {
+        Random rand = new Random();
+        int crossoverPoint = rand.Next(LBnP*3 -2 );
+
+        string child = parent1.Parameter.Substring(0, crossoverPoint) + parent2.Parameter.Substring(crossoverPoint);
+        Console.WriteLine(parent1.Parameter);
+        Console.WriteLine(parent2.Parameter);
+        Console.WriteLine(child);
+        return new Object(child);
+    }
+
+    public static List<Object> Cross(List<Object> objects)
+    {
+        List<Object> crossed_list = new();
+        Object specimen1 = objects[0];
+        Object specimen2 = objects[1];
+        Object specimen3 = objects[2];
+        Object specimen4 = objects[3];
+        Object specimen9 = objects[8];
+        Object specimen10 = objects[9];
+        Object firstFromLast = objects[objects.Count - 2];
+        Object last = objects[objects.Count -1];
+
+        crossed_list.Add(Crossover(specimen1, specimen2));
+        crossed_list.Add(Crossover(specimen3, specimen4));
+        crossed_list.Add(Crossover(specimen9, specimen10));
+        crossed_list.Add(Crossover(firstFromLast, last));
+        return crossed_list;
     }
 
     public static Object HotDeckSelection(List<Object> objects)
@@ -163,7 +181,8 @@ class Program
                 bestObject = item;
             }
         } 
-        return bestObject;
+
+        return new Object(bestObject);
     }
 
     public static double GetMeanObjectValue(List<Object> objects)
@@ -181,44 +200,40 @@ class Program
         // Console.Write("Podaj liczbe bitow na parametr: ");
         // Program.LBnP = int.Parse(Console.ReadLine());
 
-        // Console.Write("Podaj ZDmin: ");
-        // Program.zdmin = int.Parse(Console.ReadLine());
-
-        // Console.Write("Podaj ZDmax: ");
-        // Program.zdmax = int.Parse(Console.ReadLine());
 
         List<string> bytesStrings = GenerateBytesStrings(LBnP);
         var bytesStringsValues = GenerateValues(bytesStrings, zdmax, zdmin);
 
         List<Object> objects = CreateObjects();
         List<Object> selectedObjects = [];
-        foreach (Object item in objects)
-            {
-                item.Rate(bytesStringsValues, LBnP);
-            }
-        for (int j=0; j<20;j++)
-        {   
-            for (int i=0; i < numberOfObjects-1;i++)
-            {
-                selectedObjects.Add(TournamentSelection(objects));
-            }
-            foreach (Object item in selectedObjects)
-            {
-                item.Mutate();
-            }
-            selectedObjects.Add(HotDeckSelection(objects));
-            foreach (Object item in selectedObjects)
-            {
-                item.Rate(bytesStringsValues, LBnP);
-            }
-            Console.WriteLine($"Iteracja {j}, najlepszy {HotDeckSelection(selectedObjects).Rating}, srednia {GetMeanObjectValue(selectedObjects)}");
-            objects.Clear();
-            selectedObjects.ForEach((item)=>
-            {
-                objects.Add(new Object(item));
-            });
-            selectedObjects.Clear();
-        }
+        selectedObjects = Cross(objects);
+        // foreach (Object item in objects)
+        //     {
+        //         item.Rate(bytesStringsValues, LBnP);
+        //     }
+        // for (int j=0; j<20;j++)
+        // {   
+        //     for (int i=0; i < numberOfObjects-1;i++)
+        //     {
+        //         selectedObjects.Add(TournamentSelection(objects));
+        //     }
+        //     foreach (Object item in selectedObjects)
+        //     {
+        //         item.Mutate();
+        //     }
+        //     selectedObjects.Add(HotDeckSelection(objects));
+        //     foreach (Object item in selectedObjects)
+        //     {
+        //         item.Rate(bytesStringsValues, LBnP);
+        //     }
+        //     Console.WriteLine($"Iteracja {j}, najlepszy {HotDeckSelection(selectedObjects).Rating}, srednia {GetMeanObjectValue(selectedObjects)}");
+        //     objects.Clear();
+        //     selectedObjects.ForEach((item)=>
+        //     {
+        //         objects.Add(new Object(item));
+        //     });
+        //     selectedObjects.Clear();
+        // }
         
 
 
